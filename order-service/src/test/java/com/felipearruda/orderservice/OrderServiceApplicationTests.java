95fa968aa9175127eec72c4343d184b27a7dc8ae
FelipeArruda15 <souzafelipesouza5@gmail.com>
2023-02-05 18:11:46 -0300

    Escrevendo Teste de Integração(Order Service)

--- order-service/src/test/java/com/felipearruda/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/felipearruda/orderservice/OrderServiceApplicationTests.java
@@ -1,13 +1,67 @@
 package com.felipearruda.orderservice;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.felipearruda.orderservice.dto.request.OrderItemDto;
+import com.felipearruda.orderservice.dto.request.OrderRequest;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Testcontainers
+@AutoConfigureMockMvc
 class OrderServiceApplicationTests {
 
+	@Container
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8");
+	@Autowired
+	private MockMvc mockMvc;
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+		dynamicPropertyRegistry.add("spring.datasource.username", mySQLContainer::getUsername);
+		dynamicPropertyRegistry.add("spring.datasource.password", mySQLContainer::getPassword);
+	}
+
 	@Test
-	void contextLoads() {
+	@Order(value = 0)
+	void mustCreateAnOrder() throws Exception {
+		OrderRequest orderRequest = getOrderRequest();
+		String orderRequestString = objectMapper.writeValueAsString(orderRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/order")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(orderRequestString)
+		).andExpect(status().isCreated());
+	}
+
+	private OrderRequest getOrderRequest() {
+		return OrderRequest.builder()
+				.orderItems(Arrays.asList(
+						OrderItemDto.builder()
+								.skuCode("Iphone_11")
+								.price(BigDecimal.valueOf(3400))
+								.quantity(1)
+								.build()
+				)).build();
 	}
 
 }
